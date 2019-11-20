c43e0e7a459c9c8b130848ba9d8814c7391d7936
Krystian <krystian_konecki@wp.pl>
2019-11-20 16:22:12 +0100

    Adding searchedList

--- js/script.js
+++ js/script.js
@@ -1,6 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const formInput = document.querySelector(".form__input");
+  const fromButtonIn = document.querySelector('.form__button');
+  const formSearchedCities = document.querySelector(".form__searchedCities");
+  formInput.value = "";
 
+  const loadCities = () => {
+    const repData = [];
+    return fetch("../data/pl.json")
+      .then(resp => resp.json())
+      .then(data => {
+        data.forEach(ele => {
+          repData.push(ele.city);
+        });
+        return repData;
+      })
+      .catch(err => console.log(err));
+  };
 
+  const createSingleSearchedCity = value => {
+    return `<li class="searchedCities__item">${value}, Polska</li>`;
+  };
+  const main = async () => {
+    let chosenCity = "";
+    const cities = await loadCities();
 
-    
+    formInput.addEventListener("input", event => {
+      const inputValue = event.target.value;
+      let findedCities = [];
+
+      while (formSearchedCities.firstChild) {
+        formSearchedCities.removeChild(formSearchedCities.firstChild);
+      }
+
+      cities.filter(value => {
+        if (value.toLowerCase().startsWith(inputValue.toLowerCase())) {
+          findedCities.push(value);
+        }
+      });
+
+      if (inputValue === "") findedCities = [];
+
+      if (findedCities.length === 0) {
+        formSearchedCities.classList.remove("form__searchedCities--active");
+        fromButtonIn.classList.remove('form__button--hidden');
+      } else if (findedCities.length > 0) {
+        let innerHtmlValue = "";
+        formSearchedCities.classList.add("form__searchedCities--active");
+        fromButtonIn.classList.add('form__button--hidden');
+        findedCities.forEach(element => {
+          innerHtmlValue += createSingleSearchedCity(element);
+        });
+        formSearchedCities.innerHTML = innerHtmlValue;
+      }
+
+      console.log(findedCities);
+    });
+  };
+  main();
 });
