c125a437240c3568f802d01ddfd2c03b574daec6
Krystian <krystian_konecki@wp.pl>
2019-11-25 13:37:36 +0100

    Adding RWD version of app

--- js/script.js
+++ js/script.js
@@ -37,6 +37,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+  const getWeekdayShort = (nr) => {
+    switch(nr){
+      case 0: return "niedz"; break;
+      case 1: return "pon"; break;
+      case 2: return "wt"; break;
+      case 3: return "śr"; break;
+      case 4: return "czw"; break;
+      case 5: return "pt"; break;
+      case 6: return "sob"; break;
+    }
+  }
+
   const getMonthName = (nr) => {
     switch(nr) {
       case 0: return "Styczeń"; break;
@@ -85,12 +97,75 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   }
 
+
+  const getWeather5Days = (city) => {
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${weatherApiKey}&units=metric`)
+           .then(resp => resp.json())
+           .then(data => data)
+           .catch(err => console.log(err));
+  }
+
+
+  const createListDate = (list) => {
+    let listInnerHtml = "";
+    let index = 1;
+
+    if (list.length === 4) {
+      for(let i=0;i<=4;i++){
+        if(i === 4){
+          listInnerHtml += createSingleDate(list[i],index,false);
+          console.log(i)
+                              
+        } else {
+          listInnerHtml += createSingleDate(list[i],index,true);
+          console.log(i)
+        }        
+        index++;
+      }
+      return listInnerHtml;
+    } else if (list.length > 4) {
+      list.forEach(ele => {
+        listInnerHtml += createSingleDate(ele, index,true);
+        index++;
+      });
+      return listInnerHtml;
+    }
+  }
+
+  const createSingleDate = (value,index,flag) => {
+      const today = new Date();
+      const tomorrow = new Date();
+      tomorrow.setDate(today.getDate() + index);
+      console.log(value)
+      return `<div class="weatherDaysContainer__item">
+                <h3 class="weatherDaysContainer__weekDay">${getWeekdayShort(tomorrow.getDay())}</h3>
+                <h3 class="weatherDaysContainer__dayMonth">${tomorrow.getDate()} ${getMonthName(tomorrow.getMonth()).slice(0,3)}</h3>
+                ${flag === true ?
+                 `<img class="weatherDaysContainer__img" src="../images/${getWeather(value.weather[0].icon).iconNr}.png" alt="Weather img">
+                  <p class="weatherDaysContainer__temp">temp. max </br><span class="weatherDaysContainer__temp--bold">${(value.main.temp_max).toFixed(0)} <sup style="font-size:xx-small; vertical-align:super; font-size: 15px;">o</sup>C</p>
+                  <p class="weatherDaysContainer__pressure">ciśnienie </br><span class="weatherDaysContainer__temp--bold">${(value.main.pressure).toFixed(0)} hPa</p>` 
+                  : `<h3 class="weatherDaysContainer__noItems">Brak danych</h3>`}
+                
+                </div>`
+      
+  }
+
+
   const createMainContent = async value => {
 
     const date = new Date();
     const respData = await loadWeather(value);
     const weather = getWeather(respData.weather[0].icon)
+    const weather5DaysAll = await getWeather5Days(value);
+    const weather5DaysData = [];
 
+    weather5DaysAll.list.forEach((ele) => {
+      if(ele.dt_txt.slice(11,13) === "12") {
+        if(ele.dt_txt.slice(0,10) !== `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`) {
+          weather5DaysData.push(ele);
+        }
+      }
+    })
 
     mainContent.innerHTML = `
       <div class="mainContent__fullDate"> 
@@ -101,9 +176,19 @@ document.addEventListener("DOMContentLoaded", function() {
       <h2 class="mainContent__city">${value}</h2>
       <h3 class="mainContent__weatherDescribe">${weather.text}</h3>
       <div class="mainContent__weatherDataContainer weatherDataContainer">
-          <div class="weatherDataContainer__item weatherDataContainer__temp">${respData.main.temp.toFixed(1)} <sup style="font-size:xx-small; vertical-align:super; font-size: 19px; display: block">o</sup>C</div>
-          <div class="weatherDataContainer__item">b</div>
-          <div class="weatherDataContainer__item weatherDataContainer__item--last">c</div>
+          <div class="weatherDataContainer__item weatherDataContainer__temp">${respData.main.temp.toFixed(1)} <sup style="font-size:xx-small; vertical-align:super; font-size: 15px;">o</sup>C</div>
+          <ul class="weatherDataContainer__item weatherDataContainer__list">
+            <li class="weatherDataContainer__listItem">temp. min ${respData.main.temp_min.toFixed(1)} <sup style="font-size:xx-small; vertical-align:super; font-size: 14px;">o</sup>C</li>
+            <li class="weatherDataContainer__listItem">temp. max ${respData.main.temp_max.toFixed(1)} <sup style="font-size:xx-small; vertical-align:super; font-size: 14px;">o</sup>C</li>
+          </ul>
+          <ul class="weatherDataContainer__item weatherDataContainer__item--last">
+            <li class="weatherDataContainer__listItem">ciśnienie </br> <span class="weatherDataContainer__listItem--bold">${respData.main.pressure} hPa</span></li>
+            <li class="weatherDataContainer__listItem">wilgotność </br> <span class="weatherDataContainer__listItem--bold">${respData.main.humidity} %</span></li>
+            <li class="weatherDataContainer__listItem">wiatr </br> <span class="weatherDataContainer__listItem--bold">${respData.wind.speed} km/h</span></li>
+          </ul>
+      </div>
+      <div class="mainContent__weatherDaysContainer weatherDaysContainer">
+        ${createListDate(weather5DaysData)}
       </div>
     `
   }
@@ -195,7 +280,6 @@ document.addEventListener("DOMContentLoaded", function() {
     asideIcon.addEventListener('click', () => {
           document.querySelector(".aside").classList.add("aside--hide");
           document.querySelector(".main").classList.add("main--active");       
-          
     })
 
   };
